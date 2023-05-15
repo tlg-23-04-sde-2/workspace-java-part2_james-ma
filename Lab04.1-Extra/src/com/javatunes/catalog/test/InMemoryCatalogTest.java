d9fcd2d8b1b9f28880aa5445e395b116c53b4c0d
jianxing-ma <jianxing.ma@tlgcohort.com>
2023-05-15 11:31:10 -0400

    Initial commit

---
+++ Lab04.1-Extra/src/com/javatunes/catalog/test/InMemoryCatalogTest.java
@@ -0,0 +1,75 @@
+/*
+ * This code is sample code, provided as-is, and we make no
+ * warranties as to its correctness or suitability for any purpose.
+ *
+ * We hope that it's useful to you.  Enjoy.
+ * Copyright LearningPatterns Inc.
+ */
+
+package com.javatunes.catalog.test;
+
+import com.javatunes.catalog.InMemoryCatalog;
+import com.javatunes.catalog.MusicCategory;
+import com.javatunes.catalog.MusicItem;
+
+import java.util.Collection;
+
+class InMemoryCatalogTest {
+
+    /*
+     * One by one, complete each test method below, and then "activate" it by
+     * uncommenting the call to that method in main().
+     *
+     * Once you see that the test method verifies the corresponding business method
+     * works correctly, you can comment out that call in main() and proceed to the next one.
+     */
+    public static void main(String[] args) {
+
+        InMemoryCatalog catalog = new InMemoryCatalog();
+
+        // testFindById();
+        // testFindByKeyword();
+        // testFindByCategory();
+        // testSize();
+        // testGetAll();
+    }
+
+    private static void testGenreCount() {
+
+    }
+
+    private static void testFindById() {
+        InMemoryCatalog catalog = new InMemoryCatalog();
+        MusicItem itemFound = catalog.findById(6L);
+        System.out.println(itemFound);
+
+        MusicItem itemNotFound = catalog.findById(19L);
+        System.out.println(itemNotFound);
+    }
+
+    private static void testFindByKeyword() {
+    }
+
+    private static void testFindByCategory() {
+        InMemoryCatalog catalog = new InMemoryCatalog();
+        Collection<MusicItem> items = catalog.findByCategory(MusicCategory.POP);
+        dump(items);
+    }
+
+    private static void testSize() {
+        InMemoryCatalog catalog = new InMemoryCatalog();
+        System.out.println("The size of the catalog is: " + catalog.size());
+    }
+
+    private static void testGetAll() {
+        InMemoryCatalog catalog = new InMemoryCatalog();
+        dump(catalog.getAll());
+    }
+
+    // a helper method to dump a Collection<MusicItem> vertically
+    private static void dump(Collection<MusicItem> items) {
+        for (MusicItem item : items) {
+            System.out.println(item);
+        }
+    }
+}
